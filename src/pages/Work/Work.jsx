9bb147488d0e370faddf108fda1af8c872cb3d4c
Doug Jones <th3dougler@gmail.com>
2022-09-25 11:44:54 -0400

    update for deployed fly apps

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -30,27 +30,17 @@ let data = [
     subheader: "Customer loyalty platform",
     dir: "/nhd",
     thumbs: 7,
-    deployed: "https://hotnotdog.herokuapp.com/",
+    deployed: "https://nothotdog.fly.dev/",
     github: "https://github.com/imaustinim/notHotDog",
     content:
       "MERN stack SPA.  This is a free, and easy-to-use loyalty rewards manager where users can manage cards, coupons, and tickets in one place. Using Material UI, Socket.io, and QR code libraries",
   },
   {
-    title: "Spotter",
-    subheader: "Crowdsourced free parking app",
-    dir: "/spotter",
-    thumbs: 7,
-    deployed: "https://spotter-demo.herokuapp.com/",
-    github: "https://github.com/th3dougler/freepark",
-    content:
-      "Python Django App built with Amazon RDS and S3.\n\n  Built and deployed in one week - collaboratively developed by myself, one other dev and two UX/UI students.\n\n Using client side JS library Leaflet, implemented Open Street Map api, as well as GeoCage api for location-based searching. I also created all database routes and controller functions.\n\n Using client side ajax/hooks, reduced page reloads and streamline interface and DOM manipulation.\n\nUsing Amazon AWS services, we configured a relational database for user data as well as an S3 bucket for fast and simple image storage.",
-  },
-  {
     title: "breadBox",
     subheader: "Bread recipe development tool",
     dir: "/breadbox",
-    thumbs: 7,
-    deployed: "https://breadbox.herokuapp.com/",
+    thumbs: 6,
+    deployed: "https://breadbox.fly.dev/",
     github: "https://github.com/th3dougler/breadBox",
     content:
       "Created with Node, Express, Javascript and MongoDB - implements EJS templating and RESTful routes.\n\nDesigned for people who are very serious about bread recipes. This tool gives you the power to build complex bread recipes involving sourdough and all manner of prefermentswith no spreadsheet skills required.\n\nUsers can fuzzy search a comprehensive list of common baking ingredients, or add their own to the custom inventory for ease of search and access.  Uses Tabulator JS Library to manage data tables, calculate bakers percentages and determine yeild",
@@ -80,18 +70,15 @@ export default function Work(props) {
             Projects
           </Typography>
         </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
+        <Grid item xs={12} sm={12} lg={4}>
           <ProjectCard data={data[0]} />
         </Grid>
 
-        <Grid item xs={12} sm={6} lg={3}>
+        <Grid item xs={12} sm={12} lg={4}>
           <ProjectCard data={data[1]} />
         </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
+        <Grid item xs={12} sm={12} lg={4}>
           <ProjectCard data={data[2]} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <ProjectCard data={data[3]} />
         </Grid>
       </Grid>
     </Box>
